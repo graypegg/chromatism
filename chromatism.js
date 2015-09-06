3d207cab6fff9015201167f8842c67e3d3981118
Graham Pegg (MacBook) <graham@citrusmedia.com>
2015-09-05 20:56:03 -0400

    Fade is now a RGB fade

--- chromatism.js
+++ chromatism.js
@@ -44,26 +44,26 @@ function convert( from, to, value ) {
 function fade( mode, amount, fromRef, toRef ) {
 	amount = amount - 1;
 	var colours = [fromRef];
-	if (mode != "hsl") {
-		fromColour = convert( mode, "hsl", fromRef );
-		toColour = convert( mode, "hsl", toRef );
+	if (mode != "rgb") {
+		fromColour = convert( mode, "rgb", fromRef );
+		toColour = convert( mode, "rgb", toRef );
 	} else {
-		fromColour = {h:fromRef.h, s:fromRef.s, l:fromRef.l};
-		toColour = {h:toRef.h, s:toRef.s, l:toRef.l};
+		fromColour = {r:fromRef.r, g:fromRef.g, b:fromRef.b};
+		toColour = {r:toRef.r, g:toRef.g, b:toRef.b};
 	}
-	hueDiff = (toColour.h - fromColour.h) / (amount);
-	satDiff = (toColour.s - fromColour.s) / (amount);
-	lightDiff = (toColour.l - fromColour.l) / (amount);
+	rDiff = (toColour.r - fromColour.r) / (amount);
+	gDiff = (toColour.g - fromColour.g) / (amount);
+	bDiff = (toColour.b - fromColour.b) / (amount);
 	var colour = fromColour;
 	for(i=0;i<(amount-1);i++) {
-		colour.h = (colour.h + hueDiff) % 360;
-		colour.s = slopeMod(colour.s + satDiff, 100);
-		colour.l = slopeMod(colour.l + lightDiff, 100);
-		if (mode != "hsl") {
-			tempColour = convert( "hsl", mode, colour );
+		colour.r = slopeMod(colour.r + rDiff, 255);
+		colour.g = slopeMod(colour.g + gDiff, 255);
+		colour.b = slopeMod(colour.b + bDiff, 255);
+		if (mode != "rgb") {
+			tempColour = convert( "rgb", mode, colour );
 			colours.push(tempColour);
 		} else {
-			colours.push({h:colour.h, s:colour.s, l:colour.l});
+			colours.push({r:colour.r, g:colour.g, b:colour.b});
 		}
 	}
 	colours.push(toRef);
