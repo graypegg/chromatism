59d59ae9fd78549511b3d73b9ea34c23c6bf058e
Graham Pegg (MacBook) <graham@citrusmedia.com>
2016-06-18 11:56:48 -0400

    Fixed multiply

--- src/operations/multiply.js
+++ src/operations/multiply.js
@@ -1,9 +1,9 @@
 function multiply( colourRefOne, colourRefTwo ) {
-	var c1 = convert( "hsl", colourRef );
+	var c1 = convert( "hsl", colourRefOne );
 	var c2 = convert( "hsl", colourRefTwo );
 
-	var colour = {h: c1.h, s: c2.s, l: c1.l * c2*l};
-	colour.l = (colour.l > 255 ? 255 : colour.l);
+	var colour = {h: c1.h, s: c1.s, l: 100*((c1.l/100) * (c2.l/100))};
+	colour.l = (colour.l > 100 ? 100 : colour.l);
 	colour.l = (colour.l < 0 ? 0 : colour.l);
 
 	return ready( colour );
