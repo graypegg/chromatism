f45da29129b6c9e4dbc62d080ed5c6d2804aa23e
Graham Pegg (MacBook) <graham@citrusmedia.com>
2017-03-04 19:47:59 -0500

    Working on cleaning up this lib a bit, first going to import conversions + operations as groups

---
+++ src/conversions/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  fromCmyk: require('./fromCmyk.js'),
+  fromCssHsl: require('./fromCssHsl.js'),
+  fromCssRgb: require('./fromCssRgb.js'),
+  fromHex: require('./fromHex.js'),
+  fromHsl: require('./fromHsl.js'),
+  fromHsv: require('./fromHsv.js'),
+  fromRgb: require('./fromRgb.js'),
+  fromYiq: require('./fromYiq.js')
+}
