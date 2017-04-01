d6792bcf14369e897a615d55ca3822f44aca2cde
Graham Pegg (MacBook) <graham@citrusmedia.com>
2017-04-01 15:59:34 -0400

    Much better way of running tests, have some issues to fix on conversions

--- src/conversions/fromHex.js
+++ src/conversions/fromHex.js
@@ -13,7 +13,7 @@ function fromHex( { conversions, operations, helpers }, to, value ) {
 			break;
 		/* This colour mode is just an expression of RGB */
 		default:
-			return operations.convert({ conversions, helpers }, to, {
+			return operations.convert({ conversions, operations, helpers }, to, {
 				r: value[0],
 				g: value[1],
 				b: value[2]
---
+++ test/consts.js
@@ -0,0 +1,12 @@
+const red = {
+  rgb: { r: 255, g: 0, b: 0 },
+  hex: '#ff0000',
+  cssrgb: 'rgb(255,0,0)',
+  hsl: { h: 0, s: 100, l: 50 },
+  csshsl: 'hsl(0,100%,50%)',
+  hsv: { h: 0, s: 100, v: 100 },
+  cmyk: { c: 0, m: 1, y: 1, k: 0 },
+  yiq: { y: 0.299, i: 0.5957, q: 0.211 }
+}
+
+module.exports = { red }
