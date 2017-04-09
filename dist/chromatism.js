8b9ece79756277409543052a96469a5bc586a6be
Graham Pegg (MacBook) <graham@citrusmedia.com>
2017-04-09 15:12:48 -0400

    Removed weird fromHSV edgecase when h was 0

--- dist/chromatism.js
+++ dist/chromatism.js
@@ -585,50 +585,46 @@ function fromHsv(_ref, to, value) {
 			value.h = value.h / 360;
 			value.s = value.s / 100;
 			value.v = value.v / 100;
-			if (value.h == 0) {
-				r = value.v * 255;
-				g = value.v * 255;
-				b = value.v * 255;
+
+			var hsix = value.h * 6;
+			if (hsix == 6) hsix = 0;
+			var i = Math.round(hsix);
+			var var_1 = value.v * (1 - value.s);
+			var var_2 = value.v * (1 - value.s * (hsix - i));
+			var var_3 = value.v * (1 - value.s * (1 - (hsix - i)));
+
+			var r2, g2, b2;
+
+			if (i == 0) {
+				r2 = value.v;
+				g2 = var_3;
+				b2 = var_1;
+			} else if (i == 1) {
+				r2 = var_2;
+				g2 = value.v;
+				b2 = var_1;
+			} else if (i == 2) {
+				r2 = var_1;
+				g2 = value.v;
+				b2 = var_3;
+			} else if (i == 3) {
+				r2 = var_1;
+				g2 = var_2;
+				b2 = value.v;
+			} else if (i == 4) {
+				r2 = var_3;
+				g2 = var_1;
+				b2 = value.v;
 			} else {
-				var hsix = value.h * 6;
-				if (hsix == 6) hsix = 0;
-				var i = Math.round(hsix);
-				var var_1 = value.v * (1 - value.s);
-				var var_2 = value.v * (1 - value.s * (hsix - i));
-				var var_3 = value.v * (1 - value.s * (1 - (hsix - i)));
-
-				var r2, g2, b2;
-
-				if (i == 0) {
-					r2 = value.v;
-					g2 = var_3;
-					b2 = var_1;
-				} else if (i == 1) {
-					r2 = var_2;
-					g2 = value.v;
-					b2 = var_1;
-				} else if (i == 2) {
-					r2 = var_1;
-					g2 = value.v;
-					b2 = var_3;
-				} else if (i == 3) {
-					r2 = var_1;
-					g2 = var_2;
-					b2 = value.v;
-				} else if (i == 4) {
-					r2 = var_3;
-					g2 = var_1;
-					b2 = value.v;
-				} else {
-					r2 = value.v;
-					g2 = var_1;
-					b2 = var_2;
-				}
-
-				r = r2 * 255;
-				g = g2 * 255;
-				b = b2 * 255;
+				r2 = value.v;
+				g2 = var_1;
+				b2 = var_2;
 			}
+
+			r = r2 * 255;
+			g = g2 * 255;
+			b = b2 * 255;
+
 			return { r: r, g: g, b: b };
 			break;
 		case "css-rgb":
