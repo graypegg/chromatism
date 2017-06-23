e9e2167fc3d46ce50af4fdd12f50f5186204f167
Josh Duff <me@JoshDuff.com>
2017-06-23 17:49:55 -0500

    var -> const/let

--- src/operations/triad.js
+++ src/operations/triad.js
@@ -2,10 +2,10 @@ import makeColourObject from './convert'
 import convert from '../helpers/convert-to-type'
 
 export default function triad (colourRef) {
-  var colour = convert('hsl', colourRef)
+  const colour = convert('hsl', colourRef)
 
-  var colours = [{ h: colour.h, s: colour.s, l: colour.l }]
-  for (var i = 0; i < 2; i++) {
+  const colours = [{ h: colour.h, s: colour.s, l: colour.l }]
+  for (let i = 0; i < 2; i++) {
     colour.h = (colour.h + 120) % 360
     colours.push({ h: colour.h, s: colour.s, l: colour.l })
   }
