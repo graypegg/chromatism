b557a54a8242ff536b10f650dbad35d61d863bc8
Josh Duff <me@JoshDuff.com>
2017-06-23 17:49:47 -0500

    Make the build more efficient, make the ESM export point at source
    Putting both targets in the rollup config file makes the build faster.
    
    Pointing pkg.module at the live code allows for hawt tree-shaking action.

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,22 @@
-import path from 'path'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
 import uglify from 'rollup-plugin-uglify'
 import { minify } from 'uglify-es'
 
+const pkg = require('./package.json')
+
 export default {
-  entry: path.resolve('src', 'index.js'),
+  entry: pkg.module,
   moduleName: 'chromatism',
   plugins: [
     commonjs(),
     resolve(),
     babel(),
     uglify({}, minify)
+  ],
+  targets: [
+    { dest: pkg.main, format: 'umd' },
+    { dest: pkg.browser, format: 'cjs' }
   ]
 }
