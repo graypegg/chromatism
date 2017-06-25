518ffbf7609066330eda0773aba89ac49ff284a6
Josh Duff <me@JoshDuff.com>
2017-06-25 16:35:08 -0500

    Re-fix merge + autoformat

--- src/helpers/convert-to-type.js
+++ src/helpers/convert-to-type.js
@@ -34,35 +34,23 @@ export default function convert (toType, value, currentType) {
     throw new Error('No value provided')
   }
 
-  if (Array.isArray(value)) {
-    // If value passed in is an array, then recurse over the array.
+  const fromType = currentType || determineType(value)
 
-    value.map((colour) => (
-      convert(toType, colour)
-    ))
-    return colour
-  } else {
-    // If value is NOT an array, convert to new colour mode as long as
-    // source + destination colour modes are different.
-
-    const fromType = currentType || determineType(value)
-
-    if (fromType === toType) {
-      return value
-    }
-
-    if (conversions[fromType][toType]) {
-      return convertNow(fromType, toType, value)
-    }
-
-    const possibleSteps = conversionSteps[fromType]
-    const nextStepType = typeof possibleSteps === 'string'
-      ? possibleSteps
-      : (possibleSteps[toType] || possibleSteps.default)
-
-    const convertedToNextStep = convertNow(fromType, nextStepType, value)
-    return convert(toType, convertedToNextStep, nextStepType)
+  if (fromType === toType) {
+    return value
   }
+
+  if (conversions[fromType][toType]) {
+    return convertNow(fromType, toType, value)
+  }
+
+  const possibleSteps = conversionSteps[fromType]
+  const nextStepType = typeof possibleSteps === 'string'
+    ? possibleSteps
+    : (possibleSteps[toType] || possibleSteps.default)
+
+  const convertedToNextStep = convertNow(fromType, nextStepType, value)
+  return convert(toType, convertedToNextStep, nextStepType)
 }
 
 const convertNow = (fromType, toType, value) => conversions[fromType][toType](value)
